a8ff93d7bf9bcdb11421da2edeacb08d48ec2aee
Annecle <43521648+Annecle@users.noreply.github.com>
2020-10-10 13:24:19 +0800

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class HelloWorld {
+  String str;
+  public void hello() {
+    str = "Hello World!";
+  }
+  public String getStr() {
+    return str;
+  }
+  public static void main(String []args)
+  {
+  	System.out.printf("Hello World!");
+  }
+}
